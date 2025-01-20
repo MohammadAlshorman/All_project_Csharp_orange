ead9d61cf0acb87f2039ec38d8d1795c936dd0b0
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-01-20 20:14:01 +0300

    SOLVE TASK 20.1.2025

--- 19.1.2025/tasks/tasks/obj/Release/net8.0/tasks.AssemblyInfo.cs
+++ 19.1.2025/tasks/tasks/obj/Release/net8.0/tasks.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("tasks")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c728b544549dcd3b9bda5592e67472e5e40e799")]
 [assembly: System.Reflection.AssemblyProductAttribute("tasks")]
